c6bd10e40479703147211c7a010bee76f70e854f
Edgar Silva <emariano@uolinc.com>
2025-02-06 20:16:33 -0300

    Adicionado a lingua francesa no hello.py

--- hello.py
+++ hello.py
@@ -36,8 +36,10 @@ msg = "Hello, World"
 
 if current_language ==  "pt_BR":
     msg = "Olá, Mundo!"
-if current_language == "it_IT":
+elif current_language == "it_IT":
     msg = "Ciao, Mondo"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
 
 
 print(msg)
